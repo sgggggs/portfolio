2f5b4c32383b9e87cc74ab986fee0612989f2a53
sg <sg.web.st@gmail.com>
2023-08-17 13:39:18 -0700

    chore: Introducing prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'unused-imports'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:jsx-a11y/recommended',
+		'next/core-web-vitals',
+		'prettier',
+	],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'error',
+		'no-unused-vars': 'off',
+		'unused-imports/no-unused-imports': 'warn',
+		'unused-imports/no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				varsIgnorePattern: '^_',
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'type'],
+				pathGroups: [
+					{
+						pattern: '{.,..}/**/*.css',
+						group: 'type',
+						position: 'after',
+					},
+				],
+				'newlines-between': 'never',
+				alphabetize: { order: 'asc', caseInsensitive: true },
+			},
+		],
+	},
+};
