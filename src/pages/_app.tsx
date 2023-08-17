c0b2bccbf41bd14a77bb7d5c68f4bff31e198f82
sg <sg.web.st@gmail.com>
2023-08-17 14:44:54 -0700

    chore: Introducing a11y axe-core

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import { reportAccessibility } from '../lib/axe/reportAccessibility';
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
 	return <Component {...pageProps} />;
 }
+
+void reportAccessibility(React);
