af29281697baf31d62c816a0e8bd6402f9ccd8f7
sg <sg.web.st@gmail.com>
2023-08-18 22:33:23 -0700

    chore: change docs

--- src/utils/isIncluded.ts
+++ src/utils/isIncluded.ts
@@ -1,7 +1,7 @@
 /**
  * @summary Type guard the received array into a Union type
- * @param {readonly any[]} arr Array containing values of the target Union type for refinement
- * @param {any} arg Target variable
+ * @param {readonly T[]} arr Array containing values of the target Union type for refinement
+ * @param {unknown} arg Target variable
  */
 export const isIncluded = <const T>(arr: readonly T[], arg: unknown): arg is T =>
 	arr.includes(arg as T);
