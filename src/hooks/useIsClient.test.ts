3306ca34c63379f4d5b3d4150738cbcd8eb858cd
sg <sg.web.st@gmail.com>
2023-08-26 21:03:55 -0700

    chore: update test mock

--- src/hooks/useIsClient.test.ts
+++ src/hooks/useIsClient.test.ts
@@ -2,19 +2,19 @@ import { renderHook as renderHookCsr } from '@testing-library/react-hooks/dom';
 import { renderHook as renderHookSsr } from '@testing-library/react-hooks/server';
 import { useIsClient } from './useIsClient';
 
-describe('useIsClient()', () => {
-	test('should be false when rendering on the server', (): void => {
+describe('useIsClient', () => {
+	test('should be false when rendering on the server', () => {
 		const { result } = renderHookSsr(() => useIsClient());
 		expect(result.current).toBe(false);
 	});
 
-	test('should be true when after hydration', (): void => {
+	test('should be true when after hydration', () => {
 		const { result, hydrate } = renderHookSsr(() => useIsClient());
 		hydrate();
 		expect(result.current).toBe(true);
 	});
 
-	test('should be true when rendering on the client', (): void => {
+	test('should be true when rendering on the client', () => {
 		const { result } = renderHookCsr(() => useIsClient());
 		expect(result.current).toBe(true);
 	});
